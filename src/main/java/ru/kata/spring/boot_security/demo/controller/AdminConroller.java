aeaa2906776b96d915b456ac3251182a63239522
Sukharev-K <kostia.sukharev.a@gmail.com>
2023-11-28 22:55:53 +0300

    Исправлено

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminConroller.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminConroller.java
@@ -1,7 +1,17 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.servlet.ModelAndView;
 import ru.kata.spring.boot_security.demo.exception.DuplicateLoginException;
 import ru.kata.spring.boot_security.demo.model.User;
